e01355cbd18fc49bc9163bcb2e8ca7d9d0b2225a
jason111nn <jason5j288@gmail.com>
2026-01-03 22:53:53 +0800

    gitpagegg

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/LogicFlow/' : '/',
+  base: process.env.NODE_ENV === 'production' || process.env.VITE_BUILD === 'true' ? '/LogicFlow/' : '/',
   build: {
     outDir: 'dist',
   },
